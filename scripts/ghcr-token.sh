4eb9a7cd696e325f05633bce018823e4d0a4d342
Abhijith Ganesh <work@abhijithganesh.com>
2025-03-15 00:56:44 +0530

    Changes to everything
    Signed-off-by: Abhijith Ganesh <work@abhijithganesh.com>

---
+++ scripts/ghcr-token.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry ghcr-secret \
+  --docker-server=ghcr.io \
+  --docker-username=abhijithganesh \
+  --docker-password=<GHCR_TOKEN>
+  
---
+++ scripts/istio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/manifests/charts/base/files/crd-all.gen.yaml
