d2015251d1a434541becf055776db3f3f509e69e
Abhijith Ganesh <work@abhijithganesh.com>
2025-03-15 08:24:29 +0000

    Update

--- scripts/installgpf.sh
+++ scripts/installgpf.sh
@@ -8,19 +8,9 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo "Creating Monitoring workspace"
-kubectl create ns monitoring
+kubectl get ns monitoring || kubectl create ns monitoring
 
 ./prometheus/install.sh && \
 ./grafana/install.sh && \
 ./loki/install.sh
 
-echo "Port forwarding Grafana to localhost:3000..."
-kubectl port-forward -n monitoring svc/grafana 3000:80 &
-
-echo "Port forwarding Loki to localhost:3100..."
-kubectl port-forward -n monitoring svc/loki 3100:80 &
-
-echo "Port forwarding Prometheus to localhost:3200..."
-kubectl port-forward -n monitoring svc/prometheus-server 3200:80 &
-
-wait
